908ed9b661fcd6cbc5c13ac277b7e303a3ab8bf0
Ann <anngineer@gmail.com>
2022-01-14 17:09:18 -0500

    Cleans up JavaScript and plans next step of JS

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -8,110 +8,31 @@
 // 
 
 
+// -----Stating the Global Constants
 
-
-
-/* When click on addButton 
-1) take the value of the input text box and save that
-2) create a new id for the new li, which will be li + lengthOfItemList
-3) make a const for the text of 
-    <li id="list-itemLENGTHOFLISTPLUSONE" class="list-item "><input type="checkbox" class="checkbox checkbox1" name="NEWCONST" id="">NEWCONST<button>x</button></li>
-4) append that on to the last list
-5) add an event listener to see if that one is completed
-and another event listener to delete/display:none;
-*/
+// Locate the newTaskInput from the textbox.
 const newTaskInput = document.getElementById('newTaskInput');
-console.dir(newTaskInput);
+
+// Locate the button for adding a new task item. 
 const addButton = document.getElementById('addButton');
-console.dir(addButton);
-// const lengthUl = document.querySelector('ul').length;
-// console.log(lengthUl);
 
+//Create an array to store active tasks to use in the future.
 const savedTaskList = [];
-console.log(savedTaskList);
-
-// The function that creates a new list item.  You might want to clean this up
-// or turn it into its own object.
-function createNewListItem() {
-
-    const newInnerText = newTaskInput.value;
-    console.log(newTaskInput.value);
-    
-    // Get the number for the new item
-    const lengthUl = document.querySelector('ul').childElementCount;
-    console.log(lengthUl);
-    const newLength = lengthUl +1;
-    console.log(newLength);
-
-    // making the new li element
-    const newID = "list-item"+newLength;
-    console.log(newID);
-    const newTaskItemElement = document.createElement('li');
-    newTaskItemElement.classList = "list-item";
-    newTaskItemElement.id = newID;
-    newTaskItemElement.innerText= newInnerText;
-    console.log(newTaskItemElement); 
-    savedTaskList.push(newInnerText);
-    console.log(savedTaskList);
 
 
-    // making the new checkmark element
-    const newCheckBox = document.createElement('input');
-    const checkBoxClasses = "checkbox checkbox"+newLength;
-    newCheckBox.classList=checkBoxClasses;
-    newCheckBox.type= "checkbox";
-    console.log(newCheckBox);
-    // Make a new event listener fot that checkbox using the id, maybe? 
-    // We might need to make this a class thing where when you hit the button it creates a whole new object with 
-    //built in functions
-    newCheckBox.addEventListener('click', ()=> {
-    newTaskItemElement.classList.toggle('completed');
-    })
+/*-------Event Listeners on the textbox and add-task buttons----*/
 
-
-
-
-
-
-    newTaskItemElement.prepend(newCheckBox);
-    const parentUL = document.querySelector('ul');
-    parentUL.appendChild(newTaskItemElement);
-
-
-
-
-    //making the new delete button
-    const newDeleteButton = document.createElement('button');
-    newDeleteButton.classList = "deleteButton";
-    newDeleteButton.innerText="x"
-    newTaskItemElement.appendChild(newDeleteButton);
-    newDeleteButton.addEventListener('click', () => {
-        const parentLI = newDeleteButton.parentElement;
-        console.log(parentLI);
-        var stringToDelete =parentLI.innerText.slice(0,-1); 
-        console.log(stringToDelete);
-        const index = savedTaskList.indexOf(stringToDelete);
-        console.log(index);
-        // console.log(savedTaskList.);
-        savedTaskList.splice(index,1);
-        console.log(savedTaskList);
-        parentLI.style.display = "none";
-
-    })
-
-
-
-
-
-}
-
+// Event listener for clicking the task add button
 addButton.addEventListener('click', () =>{
     //Create a new List Item with this information using the function above.
     createNewListItem();
     // Reset the input box. 
     newTaskInput.value="";
+    // ADD ANIMATION OF EXCITEMENT
+
 });
-// newTaskInput.addEventListener('')
+
+// Event listener for hitting enter inside the textbox
 newTaskInput.addEventListener("keyup", ({key}) => {
     if (key === "Enter") {
     //Create a new List Item with this information using the function above.
@@ -120,16 +41,77 @@ newTaskInput.addEventListener("keyup", ({key}) => {
         // Reset the input box. 
         newTaskInput.value="";
     }
+    // ADD ANIMATION OF EXCITEMENT
 })
 
-/* When clicking any of the checkboxes
- use toggle to change class to "completed" 
- or not completed when clicked */
+/*--------------------FUNCTION for creating new list items -----------------*/
 
 
+function createNewListItem() {
 
+   // Get the string for task from the text box
+    const newInnerText = newTaskInput.value;
+     
+    // Get the number for the new item's ID with the task list Ul's length
+    const lengthUL = document.getElementById('task-list-UL').childElementCount;
+    const newLength = lengthUL +1;
+  
+    // Create the new li element with an ID, classList, innerText, and then adding the innerText onto the savedTaskList array.
+    const newID = "list-item"+newLength;
+    const newTaskItemElement = document.createElement('li');
+    newTaskItemElement.classList = "list-item";
+    newTaskItemElement.id = newID;
+    newTaskItemElement.innerText= newInnerText;
+    savedTaskList.push(newInnerText);
+
+    // Create new checkmark element and add it to the li element
+    const newCheckBox = document.createElement('input');
+    const checkBoxClasses = "checkbox checkbox"+newLength;
+    newCheckBox.classList=checkBoxClasses;
+    newCheckBox.type= "checkbox";
+    // Event listener for that checkbox to toggle the class "completed" when clicked 
+    newCheckBox.addEventListener('click', ()=> {
+    newTaskItemElement.classList.toggle('completed');
+    // ADD ANIMATION OF REWARD
+    })
+    newTaskItemElement.prepend(newCheckBox);
 
  
+    //Create delete button and adding it to the end inside the li element
+    const newDeleteButton = document.createElement('button');
+    newDeleteButton.classList = "deleteButton";
+    newDeleteButton.innerText="x"
+    newTaskItemElement.appendChild(newDeleteButton);
+    //Event listener to display:none when clicked and take the element's innerText off of the saved tasks array. 
+    newDeleteButton.addEventListener('click', () => {
+        const parentLI = newDeleteButton.parentElement;
+        var stringToDelete =parentLI.innerText.slice(0,-1); 
+        const index = savedTaskList.indexOf(stringToDelete);
+        savedTaskList.splice(index,1);
+        parentLI.style.display = "none";
+        //ADD ANIMATION OF REWARD
+    })
+
+    //Insert the li element into the document 
+    const parentUL = document.getElementById('task-list-UL');
+    parentUL.appendChild(newTaskItemElement);
+
+
+
+
+    /* SUMMARY OF THIS FUNCTION 
+    it's externally called when you click on addButton or when you hit ENTER inside the textbox 
+    1) take the value of the input text box and save that
+    2) create a new id for the new li, which will be li + lengthOfItemList
+    3) make a const for the text of 
+        <li id="list-itemLENGTHOFLISTPLUSONE" class="list-item "><input type="checkbox" class="checkbox checkbox1" name="NEWCONST" id="">NEWCONST<button>x</button></li>
+    4) append that on to the last list
+    5) add an event listener to see if that one is completed
+    and another event listener to delete/display:none;
+    */
+
+}
+
 
 /*-------------------Additional Value Action--------------------*/
 
@@ -143,27 +125,28 @@ newTaskInput.addEventListener("keyup", ({key}) => {
 
 
 // 2. ANIMATIONS
+// Look for " ADD ANIMATION"
 // Add in event listeners to make the dog move when you add an item, delete an item,
 // and give it presents and make it move when you check an item off. 
+
+
+
+/* ----------ANIMATION FUNCTIONS----------------------------*/
+
 
 
 
 /* -----------------Playing around in the console------------*/
 
 
-
+// This is just a blueprint for the first task item that's automatically added in the HTML.
 var taskList = document.getElementsByClassName("list-item");
-console.dir(taskList);
-
 const taskItem = document.querySelector('li');
-console.dir(taskItem);
-
 // when clicked
 const checkbox = document.querySelector('input');
 checkbox.addEventListener('click', ()=> {
     taskItem.classList.toggle('completed');
 })
-
 // Have a click event set for the delete buttons that
 // will hide the parent list item
 const deleteButton= document.querySelector('button');
